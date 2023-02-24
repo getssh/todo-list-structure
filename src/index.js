9494ba52cd1999ed330a1285f9478030a8341921
getssh <gtamrat33@gmail.com>
2023-02-24 19:07:20 +0300

    Update index.js file to create tasks daynamically

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
 import './style.css';
 
-const todoLists = [
-  { description: 'wash the dishes', completed: true, index: 0 },
-  { description: 'complete To Do list project', completed: false, index: 1 },
-];
+const todoLists = [];
 
 const todoListEl = document.getElementById('todo-lists');
+const todoInputEl = document.getElementById('todo-input');
+const tasks = [];
 
-for (let i = 0; i < todoLists.length; i += 1) {
-  const taskWrapper = document.createElement('div');
-  const taskIcon = document.createElement('span');
-  const todoItemEl = document.createElement('li');
-  const checkboxEl = document.createElement('input');
+function renderTasks(tasks) {
+  todoListEl.innerHTML = '';
 
-  taskIcon.className = 'material-symbols-outlined';
-  taskIcon.innerHTML = 'more_vert';
+  for (let i = 0; i < tasks.length; i += 1) {
+    const taskWrapper = document.createElement('div');
+    const taskIcon = document.createElement('span');
+    const todoItemEl = document.createElement('li');
+    const checkboxEl = document.createElement('input');
 
-  checkboxEl.type = 'checkbox';
-  checkboxEl.checked = todoLists[i].completed;
-  taskWrapper.appendChild(checkboxEl);
-  const descriptionEl = document.createTextNode(todoLists[i].description);
-  taskWrapper.appendChild(descriptionEl);
-  todoItemEl.appendChild(taskWrapper);
-  todoItemEl.appendChild(taskIcon);
-  todoListEl.appendChild(todoItemEl);
+    taskIcon.className = 'material-symbols-outlined';
+    taskIcon.innerHTML = 'more_vert';
+
+    checkboxEl.type = 'checkbox';
+    checkboxEl.checked = tasks[i].completed;
+    taskWrapper.appendChild(checkboxEl);
+    const descriptionEl = document.createTextNode(tasks[i].description);
+    taskWrapper.appendChild(descriptionEl);
+    todoItemEl.appendChild(taskWrapper);
+    todoItemEl.appendChild(taskIcon);
+    todoListEl.appendChild(todoItemEl);
+  }
 }
+
+document.querySelector('.todo-input').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const newTaskDescription = todoInputEl.value.trim();
+
+  if (!newTaskDescription) {
+    return;
+  }
+
+  const newTask = {
+    description: newTaskDescription,
+    completed: false,
+    index: tasks.length,
+  };
+
+  tasks.push(newTask);
+  todoInputEl.value = '';
+
+  renderTasks(tasks);
+});
+
+renderTasks(todoLists);

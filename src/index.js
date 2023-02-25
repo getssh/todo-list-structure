f98c72cc4454e8df1cc91db60092fb589f6224f3
getssh <gtamrat33@gmail.com>
2023-02-25 10:23:21 +0300

    Add delte completed task function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './style.css';
 import renderTasks from './taskList.js';
 import addTask from './addTask.js';
 
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const todoListEl = document.getElementById('todo-lists');
 const todoInputEl = document.getElementById('todo-input');
+const clearComplete = document.getElementById('delete-complete');
 
 renderTasks(tasks, todoListEl);
 
@@ -13,4 +14,13 @@ document.querySelector('.todo-input').addEventListener('submit', (e) => {
   e.preventDefault();
   addTask(tasks, todoInputEl);
   renderTasks(tasks, todoListEl);
+});
+
+clearComplete.addEventListener('click', () => {
+  tasks = tasks.filter((task) => !task.completed);
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  renderTasks(tasks, todoInputEl);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 });
